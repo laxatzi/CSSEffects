1b27ff0e39ddc490a452c1e685628d8d780fe96b
laxatzi <laxatzi@yahoo.com>
2020-10-14 10:39:45 +0300

    ADD: add scroll top function and scrolling on top effect in scrollTop.js file

--- scrollTop.js
+++ scrollTop.js
@@ -1,1 +1,17 @@
+// select button
+const scrollTopBtn = document.getElementById('scroll_top');
+// store root element
+  // Offset values based on this elem
+const rootElem = document.documentElement;
 
+
+function scrollTop(){
+    rootElem.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+    });
+}
+
+// event listener for scrolling
+scrollTopBtn.addEventListener('click', scrollTop);
